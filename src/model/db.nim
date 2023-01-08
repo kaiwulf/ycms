06cd64138140593e72b2643be546ab992566b799
kai <eigenspaces@gmail.com>
2023-01-07 19:54:56 -0600

    Revert "commiting ycms project"
    This reverts commit 35583094166c651fffe54132c6d2c7ceb176297a.

--- src/model/db.nim
+++
@@ -1,7 +0,0 @@
-#[
-    Page for initializing database for use
-]#
-
-import sqlite
-
-var db* = open_db()
--- src/ycms.nim
+++
@@ -1,6 +0,0 @@
-# The main entry point of the yCMS.
-
-import view/main
-
-when isMainModule:
-  echo(main_page())
