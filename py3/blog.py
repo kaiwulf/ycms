f42f8b3fa2e49b7f686b03c5c73ad2e8d0aa0142
kai <eigenspaces@gmail.com>
2024-11-16 12:35:47 -0600

    added functionality to show latest 3 blog posts on main page

--- py3/blog.py
+++ py3/blog.py
@@ -8,6 +8,15 @@ from py3.db import get_db
 
 bp = Blueprint('blog', __name__)
 
+def get_latest_posts(limit=3):
+    db = get_db()
+    return db.execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' ORDER BY created DESC LIMIT ?',
+        (limit,)
+    ).fetchall()
+
 @bp.route('/blog')
 def blog_index():
     db = get_db()
--- py3/index.py
+++ py3/index.py
@@ -2,11 +2,14 @@ from flask import (
     Blueprint, render_template
 )
 
+from py3.blog import get_latest_posts
+
 bp = Blueprint('index', __name__)
 
 @bp.route('/')
 def index():
-    return render_template('/index.html')
+    latest_posts = get_latest_posts()
+    return render_template('/index.html', latest_posts=latest_posts)
 
 @bp.route('/about')
 def about():
