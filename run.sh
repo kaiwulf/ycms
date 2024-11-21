6ca8a2154ca2cddd52fee5ec48b4a76bc39ab0f8
kai <eigenspaces@gmail.com>
2024-11-21 17:43:08 -0600

    allow for new parameter for choosing different port

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ $1 == "debug" ]]; then
-    flask --app py3 --debug run
+    flask --app py3 --debug run -p ${2:-5000}
 else
-    flask --app py3 run
+    flask --app py3 run -p ${2:-5000}
 fi
