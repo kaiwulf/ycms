d3c87f00fcb9979233028f12811525b6278730e3
kai <eigenspaces@gmail.com>
2024-11-22 21:32:57 -0600

    ycms.sqlite: 	new posts blog.py 	updated with WTForms and bleach in order to save and post blog posts which render safe html blog.html.j2 & blog_index.html.j2 & create.html.j2 & index.html.j2: 	updated templates to render safe html code with wtforms.

--- py3/blog.py
+++ py3/blog.py
@@ -1,12 +1,31 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
+    Blueprint, flash, g, redirect, render_template, request, url_for, abort
 )
 from werkzeug.exceptions import abort
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField
+from wtforms.validators import DataRequired
+from bleach import clean
+from markupsafe import Markup
 
 from py3.auth import login_required
 from py3.db import get_db
 
 bp = Blueprint('blog', __name__)
+
+ALLOWED_TAGS = [
+    'a', 'b', 'abbr', 'blockquote', 'code', 'em', 'i', 'li', 'ol', 'strong', 'ul', 'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'br'
+]
+
+ALLOWED_ATTRS = {
+    'a': ['href', 'title'],
+    'abbr': ['title'],
+    'abbr': ['title']
+}
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    body = TextAreaField('Content', validators=[DataRequired()])
 
 def get_latest_posts(limit=3):
     db = get_db()
@@ -30,26 +49,42 @@ def blog_index():
 @bp.route('/blog/create', methods=('GET','POST'))
 @login_required
 def create():
-    if request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
-        error = None
+    form = PostForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        # title = request.form['title']
+        # body = request.form['body']
+        # error = None
 
-        if not title:
-            error = 'Title required'
-        
-        if error is not None:
-            flash(error)
-        else:
-            db = get_db()
-            db.execute(
-                'INSERT INTO post (title, body, author_id)'
+        body = clean(
+            form.body.data,
+            tags=ALLOWED_TAGS,
+            attributes=ALLOWED_ATTRS,
+            strip=True
+        )
+
+        db = get_db()
+        db.execute(
+            'INSERT INTO post (title, body, author_id)'
                 ' VALUES (?, ?, ?)',
                 (title, body, g.user['id'])
-            )
-            db.commit()
-            return redirect(url_for('blog.blog_index'))
-    return render_template('blog/create.html')
+        )
+
+        # if not title:
+        #     error = 'Title required'
+        
+        # if error is not None:
+        #     flash(error)
+        # else:
+        #     db = get_db()
+        #     db.execute(
+        #         'INSERT INTO post (title, body, author_id)'
+        #         ' VALUES (?, ?, ?)',
+        #         (title, body, g.user['id'])
+        #     )
+        db.commit()
+            # return redirect(url_for('blog.blog_index'))
+    return render_template('blog/create.html', form=form)
 
 def get_post(id, check_author=True):
     post = get_db().execute(
@@ -64,6 +99,10 @@ def get_post(id, check_author=True):
     
     if check_author and post['author_id'] != g.user['id']:
         abort(403)
+
+    post = dict(post)
+    post['body'] = Markup(post['body'])
+
     return post
 
 @bp.route('/blog/<int:id>/update', methods=('GET','POST'))
